c0ea45b28db2229c025ae61bfe9db74da21de3f2
alvinyanson <a.yanson@arcanys.com>
2024-08-28 16:18:40 +0800

    feat: Join operator

---
+++ Join/Program.cs
@@ -0,0 +1,90 @@
+﻿using StudentsData;
+using System.ComponentModel;
+
+namespace Join
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IList<string> StringList1 = new List<string>() 
+            { "One", "Two", "Three", "Four", "Five" };
+
+            IList<string> StringList2 = new List<string>()
+            { "One", "Two", "Three", "Six", "Seven" };
+
+
+            var result = StringList1
+                .Join(
+                StringList2, 
+                str1 => str1, 
+                str2 => str2, 
+                (str1, str2) => str1);
+             
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+            // USING METHOD SYNTAX
+            Console.WriteLine("\nUSING METHOD SYNTAX");
+            
+            var result2 = Students.SeedData() // outer sequence
+                .Join(Students.SeedData2(), // inner sequence
+                student => student.StandardId, // outer key selector
+                standard => standard.StandardID, // inner key selector
+                (student, standard) => new // result selector
+                {
+                    StudentName = student.StudentName,
+                    StandardName = standard.StandardName
+                });
+
+
+            foreach (var item in result2)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            // USING QUERY SYNTAX
+            Console.WriteLine("\nUSING QUERY SYNTAX");
+
+            var result3 = from student in Students.SeedData() // outer sequence
+                
+                join standard in Students.SeedData2() // inner sequence
+                
+                on student.StandardId equals standard.StandardID // outer left key, inner right key
+                
+                select new // result
+                {
+                    StudentName = student.StudentName,
+                    StandardName = standard.StandardName
+                };
+
+            foreach (var item in result3)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+
+
+//Points to Remember :
+
+//Join and GroupJoin are joining operators.
+
+//Join is like inner join of SQL. It returns a new collection that contains common elements from two collections whosh keys matches.
+
+//Join operates on two sequences inner sequence and outer sequence and produces a result sequence.
+
+//Join query syntax:
+
+//from... in outerSequence
+//join... in innerSequence
+//on  outerKey equals innerKey
+//select ...
--- StudentsData/StudentsData.cs
+++ StudentsData/StudentsData.cs
@@ -6,23 +6,41 @@
         {
             IList<Student> Students = new List<Student>()
             {
-                new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
-                new Student() { StudentId = 2, StudentName = "Steve",  Age = 15 } ,
-                new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
-                new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
-                new Student() { StudentId = 5, StudentName = "Ron" , Age = 19 },
-                new Student() { StudentId = 6, StudentName = "Ram" , Age = 18 }
+                new Student() { StandardId = 1, StudentId = 1, StudentName = "John", Age = 18 } ,
+                new Student() { StandardId = 1, StudentId = 2, StudentName = "Steve",  Age = 15 } ,
+                new Student() { StandardId = 1, StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                new Student() { StandardId = 2, StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+                new Student() { StandardId = 3, StudentId = 5, StudentName = "Ron" , Age = 19 },
+                new Student() { StandardId = 3, StudentId = 6, StudentName = "Ram" , Age = 18 }
             };
 
             return Students;
+        }
+
+        public static IList<Standard> SeedData2()
+        {
+            IList<Standard> StandardList = new List<Standard>() {
+                new Standard(){ StandardID = 1, StandardName="Standard 1"},
+                new Standard(){ StandardID = 2, StandardName="Standard 2"},
+                new Standard(){ StandardID = 4, StandardName="Standard 4"}
+            };
+
+            return StandardList;
         }
     }
 
     public class Student
     {
+        public int StandardId { get; set; }
         public int StudentId { get; set; }
         public string StudentName { get; set; }
         public int Age { get; set; }
     }
 
+
+    public class Standard
+    {
+        public int StandardID { get; set; }
+        public string StandardName { get; set; }
+    }
 }
