89183782c2583365a9015fe2d8c2c6bd33682f71
alvinyanson <a.yanson@arcanys.com>
2024-08-28 15:21:58 +0800

    feat: ThenBy operator

---
+++ StudentsData/StudentsData.cs
@@ -0,0 +1,28 @@
+﻿namespace StudentsData
+{
+    public class Students
+    {
+        public static IList<Student> SeedData()
+        {
+            IList<Student> Students = new List<Student>()
+            {
+                new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
+                new Student() { StudentId = 2, StudentName = "Steve",  Age = 15 } ,
+                new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+                new Student() { StudentId = 5, StudentName = "Ron" , Age = 19 },
+                new Student() { StudentId = 6, StudentName = "Ram" , Age = 18 }
+            };
+
+            return Students;
+        }
+    }
+
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+
+}
---
+++ ThenBy/Program.cs
@@ -0,0 +1,35 @@
+﻿using StudentsData;
+
+namespace ThenBy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var data = Students.SeedData();
+
+            var result = data.OrderBy(s => s.StudentName).ThenBy(s => s.Age);
+            //var result = data.OrderBy(s => s.StudentName).ThenByDescending(s => s.Age);
+
+            // USING METHOD SYNTAX
+            foreach (var student in result)
+            {
+                Console.WriteLine($"{student.StudentName} {student.Age}");
+            }
+            
+        }
+    }
+}
+
+
+//Points to Remember :
+
+//OrderBy and ThenBy sorts collections in ascending order by default.
+
+//ThenBy or ThenByDescending is used for second level sorting in method syntax.
+
+//ThenByDescending method sorts the collection in decending order on another field.
+
+//ThenBy or ThenByDescending is NOT applicable in Query syntax.
+
+//Apply secondary sorting in query syntax by separating fields using comma.
