558876626891bed51256cc47aab763499582ab42
alvinyanson <a.yanson@arcanys.com>
2024-08-28 17:07:58 +0800

    feat: Select operator

---
+++ Select/Program.cs
@@ -0,0 +1,36 @@
+﻿using StudentsData;
+
+namespace Select
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var data = Students.SeedData();
+
+
+            // USING QUERY SYNTAX
+            Console.WriteLine("USING QUERY SYNTAX");
+            var result = from s in data
+                         select new { s.StudentId, s.StudentName };
+
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            // USING METHOD SYNTAX
+            Console.WriteLine("\nUSING METHOD SYNTAX");
+
+            var result2 = data.Select(s => new { s.StudentId, s.StudentName });
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
