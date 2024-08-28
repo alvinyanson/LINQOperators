61c37df91588a5216b64446c4c6e371aacc7d687
alvinyanson <a.yanson@arcanys.com>
2024-08-28 17:16:21 +0800

    feat: All and Any operator

---
+++ AllAnyContains/Program.cs
@@ -0,0 +1,28 @@
+﻿using StudentsData;
+
+namespace AllAny
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var data = Students.SeedData();
+
+
+            // USING METHOD SYNTAX - ALL
+            Console.WriteLine("USING METHOD SYNTAX - ALL");
+            
+            var result = data.All(s => s.Age > 12 && s.Age < 20);
+
+            Console.WriteLine(result);
+
+
+            // USING METHOD SYNTAX - ANY
+            Console.WriteLine("\nUSING METHOD SYNTAX - ANY");
+            
+            var result2 = data.Any(s => s.Age > 12 && s.Age < 20);
+
+            Console.WriteLine(result2);
+        }
+    }
+}
