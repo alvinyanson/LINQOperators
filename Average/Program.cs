1571ef5aba141264c1d415b3261a264f58eda3d0
alvinyanson <a.yanson@arcanys.com>
2024-08-29 09:35:22 +0800

    feat: Average operator

---
+++ Average/Program.cs
@@ -0,0 +1,17 @@
+﻿using StudentsData;
+
+namespace Average
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var data = Students.SeedData();
+
+            var average = data.Average(x => x.Age);
+
+            Console.WriteLine(average);
+        }
+    }
+
+}
