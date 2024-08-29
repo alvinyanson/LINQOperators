7ed7aa53def75528a866f282b2e4ada8e03454ff
alvinyanson <a.yanson@arcanys.com>
2024-08-29 12:24:19 +0800

    feat: DefaultIfEmpty operator

---
+++ DefaultIfEmpty/Program.cs
@@ -0,0 +1,26 @@
+﻿using StudentsData;
+
+namespace DefaultIfEmpty
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IList<Student> emptyStudentList = new List<Student>();
+
+            var newStudentList1 = emptyStudentList.DefaultIfEmpty(new Student());
+
+            var newStudentList2 = emptyStudentList.DefaultIfEmpty(new Student()
+            {
+                StudentId = 0,
+                StudentName = ""
+            });
+
+            Console.WriteLine("Count: {0} ", newStudentList1.Count());
+            Console.WriteLine("Student ID: {0} ", newStudentList1.ElementAt(0).StudentName);
+
+            Console.WriteLine("Count: {0} ", newStudentList2.Count());
+            Console.WriteLine("Student ID: {0} ", newStudentList2.ElementAt(0).StudentId);
+        }
+    }
+}
