43ac476d0e4d4bf7a85cb9f60ff9749318392fe7
alvinyanson <a.yanson@arcanys.com>
2024-08-29 09:49:36 +0800

    feat: Count operator

---
+++ Count/Program.cs
@@ -0,0 +1,29 @@
+﻿using StudentsData;
+
+namespace Count
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var data = Students.SeedData();
+
+            // USING METHOD SYNTAX
+
+            Console.WriteLine("USING METHOD SYNTAX");
+            var count = data.Count(x => x.Age > 20);
+
+            Console.WriteLine(count);
+
+
+            // USING QUERY SYNTAX
+
+            Console.WriteLine("\nUSING QUERY SYNTAX");
+
+            var count2 = (from s in data where s.Age > 20 select s).Count();
+
+            Console.WriteLine(count2);
+        }
+    }
+}
