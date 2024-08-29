d0c3b9aa64b7d94bdce511f44f8a266ef3dfec8b
alvinyanson <a.yanson@arcanys.com>
2024-08-29 12:05:55 +0800

    feat: Concat operator

---
+++ Concat/Program.cs
@@ -0,0 +1,22 @@
+﻿using StudentsData;
+
+namespace Concat
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var data1 = Students.SeedData();
+            var data2 = Students.SeedData4();
+
+
+            // USING METHOD SYNTAX
+            var allStudents = data1.Concat(data2);
+
+            foreach (var student in allStudents)
+            {
+                Console.WriteLine(student.StudentName);
+            }
+        }
+    }
+}
