63c93ea711860bf62a31dc32a4c2d186c3fb4e74
alvinyanson <a.yanson@arcanys.com>
2024-08-29 11:42:16 +0800

    feat: Single and SingleOrDefault

---
+++ SingleAndSingleOrDefault/Program.cs
@@ -0,0 +1,34 @@
+﻿using StudentsData;
+
+namespace SingleAndSingleOrDefault
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var data = Students.SeedData();
+
+            var single = data.Single(x => x.StudentName == "John");
+
+            Console.WriteLine(single.StudentName);
+
+
+            var singleOrDefault = data.SingleOrDefault(x => x.StudentName == "John");
+
+            Console.WriteLine(singleOrDefault.StudentName);
+        }
+    }
+}
+
+
+//Points to Remember :
+
+//Single() expects one and only one element in the collection.
+
+//Single() throws an exception when it gets no element or more than one elements in the collection.
+
+//If specified a condition in Single() and result contains no element or more than one elements then it throws an exception.
+
+//SingleOrDefault() will return default value of a data type of generic collection if there is no elements in a colection or for the specified condition.
+
+//SingleOrDefault() will throw an exception if there is more than one elements in a colection or for the specified condition.
